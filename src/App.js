983a53d675754261a92a4018f967520143499326
sh0la <sholaadeniyi@gmail.com>
2020-01-18 18:52:40 -0600

    Add event handler to circle

--- src/App.js
+++ src/App.js
@@ -8,20 +8,22 @@ class CircleComponent extends Component {
   constructor(props) {
     super(props);
     this.state = {currentNum: this.generateRandom()}
+    this.updateNum = this.updateNum.bind(this);
   }
   
   generateRandom() {
     return Math.floor(Math.random() * 100) + 1;
   }
 
-  updateNum() {
+  updateNum(event) {
+    event.preventDefault();
     this.setState({currentNum: this.generateRandom()})
   }
 
   render() {
     return (
       <div id='container'>
-        <div className='circle' onClick={this.generateRandom}>
+        <div className='circle' onClick={this.updateNum}>
           <h3 className='random'>
             {this.state.currentNum}
           </h3>
